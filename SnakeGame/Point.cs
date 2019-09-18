1af2e445999bb549b4b66d1e2225ad335cdf1b36
Змиенко Евгений <snakerman7@gmail.com>
2019-09-18 16:39:24 +0300

    6. made Point properties read-only

--- SnakeGame/Point.cs
+++ SnakeGame/Point.cs
@@ -2,8 +2,8 @@
 {
     public struct Point
     {
-        public int X { get; set; }
-        public int Y { get; set; }
+        public int X { get; }
+        public int Y { get; }
 
         public Point(int x, int y)
         {
